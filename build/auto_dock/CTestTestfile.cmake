2433d46ff4f8d8e9068a32d0380ad755ccac90a5
Yusuftaya1 <tayayusuf27@gmail.com>
2025-01-06 18:54:10 +0300

    Initial commit

---
+++ build/auto_dock/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tayya/Nav2_Dock_Robot/src/auto_dock
+# Build directory: /home/tayya/Nav2_Dock_Robot/build/auto_dock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ install/auto_dock/share/auto_dock/launch/dock_test.launch.py
@@ -0,0 +1,20 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory 
+
+def generate_launch_description():
+    nova_carter_dock_params_dir = os.path.join(get_package_share_directory('auto_dock'), 'params')
+    params_file =os.path.join(nova_carter_dock_params_dir, '/home/tayya/Nav2_Dock_Robot/src/auto_dock/params/dock_server.yaml')
+
+    docking_server = Node(
+        package='opennav_docking',
+        executable='opennav_docking',
+        name='docking_server',
+        output='screen',
+        parameters=[params_file],
+    )
+
+    return LaunchDescription([
+        docking_server,
+    ])
