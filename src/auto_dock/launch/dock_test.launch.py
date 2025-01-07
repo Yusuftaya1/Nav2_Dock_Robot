131637e7bbd71f67f6bc411c9581fe7078f58c41
Yusuftaya1 <tayayusuf27@gmail.com>
2025-01-07 12:37:27 +0300

    checkpoint

--- src/auto_dock/launch/dock_test.launch.py
+++ src/auto_dock/launch/dock_test.launch.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     nova_carter_dock_params_dir = os.path.join(get_package_share_directory('auto_dock'), 'params')
-    params_file =os.path.join(nova_carter_dock_params_dir, '/home/tayya/Nav2_Dock_Robot/src/auto_dock/params/dock_server.yaml')
+    params_file = os.path.join(nova_carter_dock_params_dir, 'dock_server.yaml')
 
     docking_server = Node(
         package='opennav_docking',
--- src/auto_dock/src/test.py
+++ src/auto_dock/src/test.py
@@ -1,42 +1,61 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from rclpy.parameter import Parameter
-from std_srvs.srv import Trigger
+from rclpy.action import ActionClient
+from opennav_docking_msgs.action import DockRobot, UndockRobot
 
-class DockServerTestNode(Node):
+class RobotController(Node):
     def __init__(self):
-        super().__init__('dock_server_test_node')
-        self.client = self.create_client(Trigger, '/dock_server_ready_check')
-        
-        self.get_logger().info('Waiting for the dock server...')
-        if  self.client.wait_for_service(timeout_sec=3.0):
-            self.get_logger().error('Dock server service not available!')
-            return
+        super().__init__('robot_controller')
+        self.docking_client = ActionClient(self, DockRobot, 'dock_robot')
 
-        self.get_logger().info('Dock server service available!')
-        self.timer = self.create_timer(2.0, self.send_test_request)
 
-    def send_test_request(self):
-        self.get_logger().info('! Sending test request...')
-        request = Trigger.Request()
-        future = self.client.call_async(request)
-        future.add_done_callback(self.callback)
+    def dock_robot(self, dock_id=""):
+        self.get_logger().info("Waiting for 'DockRobot' action server...")
+        while not self.docking_client.wait_for_server(timeout_sec=1.0):
+            self.get_logger().warn('"DockRobot" action server not available, waiting...')
 
-    def callback(self, future):
-        try:
-            response = future.result()
-            if response.success:
-                self.get_logger().info(f'Test successful: {response.message}')
-            else:
-                self.get_logger().error(f'Test failed: {response.message}')
-        except Exception as e:
-            self.get_logger().error(f'Exception while calling service: {e}')
+        goal_msg = DockRobot.Goal()
+        goal_msg.dock_type = "nova_carter_dock"
+        goal_msg.use_dock_id = True
+        goal_msg.dock_id = dock_id
+        goal_msg.dock_pose.header.stamp     = self.get_clock().now().to_msg()
+        goal_msg.dock_pose.header.frame_id  = 'map' 
+        goal_msg.dock_pose.pose.position.x  = 1.0
+        goal_msg.dock_pose.pose.position.x  = 2.0
+        goal_msg.dock_pose.pose.orientation.z = 1.0
+
+        self.get_logger().info(f"Docking at ID: {dock_id}...")
+        send_goal_future = self.docking_client.send_goal_async(
+            goal_msg, feedback_callback=self._feedback_callback
+        )
+        rclpy.spin_until_future_complete(self, send_goal_future)
+        self.goal_handle = send_goal_future.result()
+
+        if not self.goal_handle.accepted:
+            self.get_logger().error("Docking request was rejected!")
+            return False
+
+        self.result_future = self.goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, self.result_future)
+
+        result = self.result_future.result().result
+        if result:
+            self.get_logger().info("Docking completed successfully!")
+        else:
+            self.get_logger().error("Docking failed!")
+        return result
+
+    def _feedback_callback(self, feedback):
+        self.get_logger().info(f"Received feedback: {feedback.feedback}")
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DockServerTestNode()
-    rclpy.spin(node)
+    node = RobotController()
+    dock_id = 'c67f50cb-e152-4720-85cc-5eb20bd85ce8'
+    node.dock_robot(dock_id)
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
